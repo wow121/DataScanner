d0b914c7d8d98ee9e1afc97626042d2e263aeab9
xianrui <469298511@qq.com>
2014-09-14 00:17:37 +0800

    update

---
+++ src/com/example/datascanner/activity/LogoActivity.java
@@ -0,0 +1,19 @@
+package com.example.datascanner.activity;
+
+import com.example.datascanner.R;
+import com.example.datascanner.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class LogoActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.logo);
+	}
+}
